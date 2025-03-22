f446988718b4671b2d9bc6dcd28c90c534d833f9
Azlia Septy Ifadah <azliaifadah@gmail.com>
2025-03-23 00:21:34 +0700

    Tugas week 4

---
+++ menu.js
@@ -0,0 +1,19 @@
+// menu.js
+import { kalkulator } from './rumus.js';
+import readline from 'readline';
+
+// Membuat interface untuk input dari terminal
+const inputUser = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Minta input dari pengguna
+inputUser.question('Masukkan angka pertama: ', (angka1) => {
+    inputUser.question('Masukkan operator (+, -, *, /): ', (operator) => {
+        inputUser.question('Masukkan angka kedua: ', (angka2) => {
+            console.log(`Hasil: ${kalkulator(parseFloat(angka1), parseFloat(angka2), operator)}`);
+            inputUser.close();
+        });
+    });
+});
